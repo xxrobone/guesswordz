5392c51ba10db1deec5627de95dac3e241b1be58
robonexx <robertwagar@gmail.com>
2022-12-05 09:00:33 +0100

    going to start on new project, rework js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-//prepping for word game "hangman"
-// 1 pick a random word from an array v
-// 2 take a players guess input using prompt v
-// 3 quit the game if player wants to quit v
-// 4 Check if player guess is a letter in the word v
-// 5 Keep track of guesses and letters guessed v
-// 6 show the player their progress v
-// 7 Show the player if the word is right v
-// 8 Create game loop v
+// keeping the old code blow, testing new code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* 
 let fetchedData = [];
 
@@ -37,20 +50,13 @@ async function fetchWords(input) {
       d.word, console.log(d.word);
     })
   );
-} */
+} 
 
-/* 
-fetch(`https://api.datamuse.com/words?ml=${topic}`)
-  .then((response) => response.json())
-  .then((data) => {
-    fetchedData.push(data.map((word) => word.word));
-  });
-console.log(fetchedData); */
 
-// button to play game
+
 const btn = document.querySelector('.btn').addEventListener('click', playGame);
 
-// only using this short array, could be more words, can change this if needed, could also use an api with words too fetch from
+
 const wordsArr = [
   'gamer',
   'coder',
@@ -95,36 +101,28 @@ const wordsArr = [
   'mile',
 ];
 
-/* const secretWord = fetchedData[Math.floor(Math.random() * fetchedData.length)]; */
 
-// creating the game loop
 function playGame() {
-  // get a random word from the words array
+ 
   const secretWord = wordsArr[Math.floor(Math.random() * wordsArr.length)];
-  // Declaring variable for player Guess
+  
   let playerGuess;
-  // creating an array to hold the right guessed letters
+  
   let answerArr = [];
-
-  // used letters array, that is not included in the word
+ 
   let usedLetters = [];
 
-  // quit game initialization set to false
   let quitGame = false;
 
-  /* looping thru the secret word and adding it to the answer array 
-so the player will see how many letters there is and
-also show progress of the word if guess is right */
   for (let i = 0; i < secretWord.length; i++) {
     answerArr[i] = '_';
   }
 
-  //creating guesses variable with 6 chances
+
   let guesses = 6;
-  // showing how many letter remain for the right word
+
   let remainingLetters = secretWord.length;
 
-  // Alert to show the info of the game
   alert(
     'Welcome! \nThis is a hangman (word guessing) game \nTo play continue with pressing OK' +
       '\nThe word is ' +
@@ -135,12 +133,12 @@ also show progress of the word if guess is right */
   );
 
   while (remainingLetters > 0 && guesses > 0) {
-    // create a variable for boolean to later add true or false for guesses control
+   
     let guessIsMatch;
-    // regex to check if input is number or letter
+   
     var regexCheckNumber = /^[0-9]+$/;
     var regexCheckLetter = /^[a-öA-Ö]+$/;
-    // initializing the playerGuess variable by taking in the player input
+   
     playerGuess = prompt(
       'Guess a letter!' +
         ' or click cancel to quit.' +
@@ -151,47 +149,47 @@ also show progress of the word if guess is right */
         '\nLetters remaining ' +
         remainingLetters
     );
-    // check if sercretword has playerguess letter in it and answer array does not
+   
     if (secretWord.includes(playerGuess) && !answerArr.includes(playerGuess)) {
-      // looping over the secretWord and checking if the playerguessed letter is in the word and what position
+     
       for (let j = 0; j < secretWord.length; j++) {
         if (secretWord[j] === playerGuess) {
           answerArr[j] = playerGuess;
-          // decrease remaining letters in the word
+         
           remainingLetters--;
-          // guess is match so guesses remains
+         
           guessIsMatch = 'match';
         }
       }
     } else if (playerGuess === null || playerGuess === '') {
-      //check player guess is
+     
       quitGame = true;
-      /* alert('no input given, game will end!'); */
+      
       break;
     } else if (
       playerGuess.length !== 1 ||
-      !playerGuess.match(regexCheckLetter) ||
-      playerGuess.match(regexCheckNumber)
+      /* !playerGuess.match(regexCheckLetter) || */
+
+    /*  playerGuess.match(regexCheckNumber)
     ) {
       alert('Please use letters only & one at a time');
     }
-    // Alert if letter is already used checking the answer array and used letters array
+    
     else if (
       answerArr.includes(playerGuess) ||
       usedLetters.includes(playerGuess)
     ) {
       alert('You already used this letter');
     } else {
-      // update the game progress guess is match to no match and -1 on guesses
-      // also push the letter that is not included in the word to used letters array
+     
       guessIsMatch = 'nomatch';
       guesses--;
       usedLetters.push(playerGuess);
     }
-    // game loop ending
+    
     console.log(guesses);
   }
-  // game endings
+  
   if (guesses === 0) {
     alert(
       'Game over \nSorry You have no more guesses: ' +
@@ -212,9 +210,9 @@ also show progress of the word if guess is right */
         answerArr.join('').toUpperCase()
     );
   }
-  //game function ending
+ 
 }
-
+ */
 // what i worked on before and keeping it here till i move it to a new project
 // ====================================================================================================================
 /* 
